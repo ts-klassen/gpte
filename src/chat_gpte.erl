77c9236335facd2cd51626c27f067b077cfb7508
ts-klassen <git@su-shiki.com>
2024-07-19 21:53:14 +0900

    default model to gpt-4o-mini

--- src/chat_gpte.erl
+++ src/chat_gpte.erl
@@ -61,7 +61,7 @@
 new() ->
     #{
         request => #{
-            model => <<"gpt-3.5-turbo">>
+            model => <<"gpt-4o-mini">>
           , temperature => 0.7
           , messages => []
         }
