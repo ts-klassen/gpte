49a8c3196d20df28a59e9d6a94d17fd26112dedf
ts-klassen <git@su-shiki.com>
2025-03-29 23:49:46 +0900

    Just commit everytihng

--- src/gpte_code2.erl
+++ src/gpte_code2.erl
@@ -26,8 +26,8 @@
     }.
 
 
--spec file(chat_gpte:chat()) -> {klsn:binstr(), chat_gpte:chat()}.
-file(Chat0) ->
+-spec file_mode(chat_gpte:chat()) -> {klsn:binstr(), chat_gpte:chat()}.
+file_mode(Chat0) ->
     FileResp = {?MODULE, file_resp, 0},
     Chat10 = chat_gpte:model(<<"o3-mini">>, Chat0),
     Chat20 = chat_gpte:schema(gpte_schema:schema(FileResp), Chat10),
@@ -44,26 +44,30 @@ file(Chat0) ->
                      Unsafe path will be rejected.
                data: The data to write.
                      The entire file will be overwritten.
-        is_done: Set true to finish.
+        is_done: Set true to finish or if there are nothing to do.
                  When files are writted, I will show you the diff.
                  If the diff is what you expected, set it to true and finish.
                  When read or write is specified, is_done must be false.
                  If nothing to do in the first place, set it to true.
                  After you reply with is_done=true, you can talk with the user.
     ">>, Chat20),
-    Loop = fun Self({Res, Chat}) ->
-        case gpte_schema:schema(FileResp, Res) of
-            #{ read := Read } when length(Read) > 0 ->
-                Self(read_file(Read, Chat));
-            #{ write := Write } when length(Write) > 0 ->
-                Self(write_file(Write, Chat));
-            #{ is_done := true } ->
-                chat_gpte:ask(<<"Exit file editing. Entered interactive mode. Your response will be displayed to the user. Simply explain how the file editing or reading went. Try not to print the entier content. Showing the diff may be a better idea.">>, chat_gpte:delete_response_format(Chat))
-        end
+    Loop = fun
+        Self({_Res, Chat}, 0) ->
+            chat_gpte:ask(<<"Too many file editing request. Aborted. Your changes have been rollbacked. Exit file editing mode. Entered interactive mode. Your response will be displayed to the user so stop responding in JSON format. Tell the user that you could not apply the changes.">>, chat_gpte:delete_response_format(Chat));
+        Self({Res, Chat}, N) ->
+            case gpte_schema:schema(FileResp, Res) of
+                #{ read := Read } when length(Read) > 0 ->
+                    Self(read_file(Read, Chat), N-1);
+                #{ write := Write } when length(Write) > 0 ->
+                    Self(write_file(Write, Chat), N-1);
+                #{ is_done := true } ->
+                    chat_gpte:ask(<<"Exit file editing mode. Entered interactive mode. Your response will be displayed to the user so stop responding in JSON format. Simply explain how the file editing or reading went. Try not to print the entier content. Showing the diff may be a better idea.">>, chat_gpte:delete_response_format(Chat))
+            end
     end,
-    Loop({Res30, Chat30}).
+    Loop({Res30, Chat30}, 10).
 
 read_file(Read, Chat) ->
+    error_logger:info_msg("read: ~p~n", [Read]),
     {ok, Cwd} = file:get_cwd(),
     Text = try
         iolist_to_binary(lists:map(fun(File)->
@@ -90,6 +94,7 @@ read_file(Read, Chat) ->
     chat_gpte:ask(Text, Chat).
 
 write_file(Write, Chat) ->
+    error_logger:info_msg("write: ~p~n", [Write]),
     {ok, Cwd} = file:get_cwd(),
     Text = try
         Errors = lists:map(fun(WriteElem)->
@@ -134,5 +139,7 @@ quote_code(File, Data, Quote) ->
         _ ->
             quote_code(File, Data, <<"`", Quote/binary>>)
     end.
+
+
 
 
